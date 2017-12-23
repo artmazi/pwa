fb3720b0db65b2f5f61a936419b16d0e0d4ad9d3
artmazi <artmazi@gmail.com>
2017-12-23 11:23:03 +0100

    starter pack added

---
+++ public/src/js/app.js
@@ -0,0 +1,27 @@
+let pwaCard = document.querySelector('#pwa'),
+    pwaCardContent = pwaCard.querySelector('.card__content'),
+    pwaCardDetails = pwaCard.querySelector('.card__details'),
+    detailsShown = false
+
+pwaCard.addEventListener('click', (e) => {
+    if (!detailsShown) {
+        detailsShown = true
+        pwaCardContent.style.opacity = 0
+        pwaCardDetails.style.display = 'block'
+        pwaCardContent.style.display = 'none'
+
+        setTimeout(() => {
+            pwaCardDetails.style.opacity = 1
+        }, 300)
+    }
+    else {
+        detailsShown = false
+        pwaCardDetails.style.opacity = 0
+        pwaCardContent.style.display = 'block'
+        pwaCardDetails.style.display = 'none'
+        setTimeout(() => {
+            pwaCardContent.style.opacity = 1;
+        }, 300)
+
+    }
+})
